5fd0ab422833c72479654468144e162f7e8693ed
pfdtk <8857485+pfdtk@users.noreply.github.com>
2023-04-08 12:07:04 +0800

    init

--- internal/queue/redis/redis_queue_test.go
+++ internal/queue/redis/redis_queue_test.go
@@ -2,30 +2,55 @@ package redis
 
 import (
 	"context"
+	"errors"
 	"github.com/pfdtk/goq/common"
 	"github.com/pfdtk/goq/internal/connect"
+	"github.com/redis/go-redis/v9"
 	"testing"
 )
 
-func TestRedisQueue_Push(t *testing.T) {
+var queueName = "default"
+
+func getQueue() *Queue {
 	c, _ := connect.NewRedisConn(&connect.RedisConf{
 		Addr:     "127.0.0.1",
 		Port:     "6379",
 		DB:       1,
 		PoolSize: 1,
 	})
-	defer func() {
-		_ = c.Close()
-	}()
 	q := NewRedisQueue(c)
+	return q
+}
+
+func TestRedisQueue_Push(t *testing.T) {
+	q := getQueue()
 	err := q.Push(context.Background(), &common.Message{
 		Type:    "test",
 		Payload: []byte("payload"),
 		ID:      "uuid-13",
-		Queue:   "default",
+		Queue:   queueName,
 		Timeout: 10,
 	})
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestQueue_Size(t *testing.T) {
+	q := getQueue()
+	s, err := q.Size(context.Background(), queueName)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("size: %d", s)
+}
+
+func TestQueue_Pop(t *testing.T) {
+	q := getQueue()
+	s, err := q.Pop(context.Background(), queueName)
+	if err != nil && !errors.Is(err, redis.Nil) {
+		t.Error(err)
+	} else {
+		t.Log(s)
 	}
 }
