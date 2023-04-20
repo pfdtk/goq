6bf7118577bb3db3a517cc4dd40e5e65d70d7211
pfdtk <8857485+pfdtk@users.noreply.github.com>
2023-04-20 16:27:49 +0800

    update

--- task/middleware.go
+++ task/middleware.go
@@ -3,3 +3,9 @@ package task
 type Middleware interface {
 	Handle(t Task, j Job) (bool, error)
 }
+
+type FuncHandler func(t Task, j Job) (bool, error)
+
+func (f FuncHandler) Handle(t Task, j Job) (bool, error) {
+	return f(t, j)
+}
